4206cf23beb3dc8044db9ff415c3c55f474192b3
s3codecL <gandradev@gmail.com>
2023-06-11 04:17:04 -0400

    carga completa de manipulación de dom

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function mostrarTexto2() {
+    document.getElementById("text2").style.display = "block";
+}
+function ocultarTexto2() {
+    document.getElementById("text2").style.display = "none";
+}
+function agrandarImagen() {
+    document.getElementById("img").style.width = "100%";
+}
+function encogerImagen() {
+    document.getElementById("img").style.width = "20%";
+}
+function aumentarLetra() {
+    document.getElementById("caja3").style.fontSize = "1.5em";
+}
